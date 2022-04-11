6da82cb6ac3d76db29922bfcd54a9dbb907635f0
Dmitry Indenbom <dmitry.indenbom@yandex.ru>
2022-04-12 00:07:53 +0300

    Storing of files was improved, but it is for one session now.

--- second_task/src/main/java/ru/netology/repository/FileRepository.java
+++ second_task/src/main/java/ru/netology/repository/FileRepository.java
@@ -3,18 +3,35 @@ package ru.netology.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static java.nio.file.Files.readAllBytes;
 
 @Repository
 public class FileRepository {
+    final private Path base_dir = Path.of("./storage/");
+
+    public FileRepository() throws IOException {
+        if (base_dir.toFile().exists()) {
+            try (Stream<Path> walk = Files.walk(base_dir)) {
+                walk.sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            }
+        }
+        Files.createDirectory(base_dir);
+        base_dir.toFile().deleteOnExit();
+    }
+
     public void saveFile(String file_name, MultipartFile file) {
         try {
-            Files.write(Path.of(file_name), file.getBytes());
+            Files.write(base_dir.resolve(file_name), file.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -23,7 +40,7 @@ public class FileRepository {
     public Optional<byte[]> getFile(String file_name) {
         byte[] file = null;
         try {
-            file = readAllBytes(Path.of(file_name));
+            file = readAllBytes(base_dir.resolve(file_name));
         } catch (IOException e) {
             e.printStackTrace();
         }
