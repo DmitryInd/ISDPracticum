7bce82632a33541ed8a00696ba6ab51856bae8aa
Дмитрий Евгеньевич Инденбом <indenbom.de@phystech.edu>
2022-04-27 15:07:00 +0300

    Bugfix

--- web-file-storage/src/main/java/ru/netology/config/WebConfig.java
+++ web-file-storage/src/main/java/ru/netology/config/WebConfig.java
@@ -1,13 +1,23 @@
 package ru.netology.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+@ComponentScan(basePackages = { "ru.netology.controller" })
+public class WebConfig implements WebMvcConfigurer {
     @Bean
     public StandardServletMultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
+    }
+
+    // equivalents for <mvc:resources/> tags
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/static/");
     }
 }
